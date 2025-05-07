66b7691ba278805bcf5d96ed4fda3fa1adf34327
pushpab24 <saisrinivasu6@gmail.com>
2025-05-06 22:22:27 -0400

    Update Recipescrapper.java

--- src/test/java/recipesTests/Recipescrapper.java
+++ src/test/java/recipesTests/Recipescrapper.java
@@ -1,160 +1,307 @@
 package recipesTests;
 
-import java.time.Duration;
-import java.util.List;
+import recipeData.recipesdetailslocator;
+import recipes_Database.Databases;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import Utils.ConfigReader;
+
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import baseclass.Baseclass;
-import recipeData.recipesdetailslocator;
+import Utils.DataProviders;
+import Utils.ExcelReader;
 
-public class Recipescrapper extends Baseclass {
-	Handling_Ads ads = new Handling_Ads();
+public class Recipescrapper extends Baseclass{
 
-	/*
-	 * Praveena private recipesdetailslocator rl;
-	 * 
-	 * @BeforeClass public void initLocator() { rl = new recipesdetailslocator(); //
-	 * safe to initialize now since driver is initialized rl.removeAds(); }
-	 * 
-	 * @Test public void navigateAndScrapeRecipes() throws InterruptedException {
-	 * 
-	 * Praveena JavascriptExecutor js = (JavascriptExecutor) driver;
-	 * js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-	 * Thread.sleep(1000);
-	 * 
-	 * WebElement recipeLink =
-	 * driver.findElement(By.xpath("//a[text()='Recipes List']"));
-	 * recipeLink.click(); Thread.sleep(1000); String recipelisttext =
-	 * driver.findElement(By.xpath("//h4[@class='rec-heading-sec']")).getText();
-	 * System.out.println("Recipe list page" + recipelisttext);
-	 */
-	@Test
-	public void navigateAndScrapeRecipes() throws InterruptedException {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    Databases DB;        
+    private List<recipesdetailslocator> allRecipes = new ArrayList<>();
+    JavascriptExecutor js;
+    private static final Logger logger = LoggerFactory.getLogger(Recipescrapper.class);
 
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
-		// Scroll all the way down
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		long initialHeight = ((Number) js.executeScript("return document.body.scrollHeight")).longValue();
-		while (true) {
-			js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-			Thread.sleep(1000); // adjust sleep based on load time
-			long newHeight = ((Number) js.executeScript("return document.body.scrollHeight")).longValue();
-			if (newHeight == initialHeight) {
-				break; // stop if height hasn't changed
-			}
-			initialHeight = newHeight;
-		}
-		WebElement recipeslink = wait
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Recipes List')]")));
-		Assert.assertTrue(recipeslink.isDisplayed(), "Element is not visible");
-		if (driver.getCurrentUrl().equals("https://www.tarladalal.com/#google_vignette")) {
-			WebElement adx = driver.findElement(By.xpath("contains(text(),'Close')]"));
-			adx.click();
-			System.out.println("closed the ad before clicking the Recipes link");
-		}
-		recipeslink.click();
+    @BeforeMethod
+    public void setUp() {
+    	driver = Baseclass.getDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        js = (JavascriptExecutor) driver;
+        DB = new Databases();        
+        DB.connectToDatabase();
+        DB.createRecipeTable(); 
+    }
 
-		if (!driver.getCurrentUrl().equals("https://www.tarladalal.com/recipes/")) {
-			System.out.println("URL mismatch! Current URL is: " + driver.getCurrentUrl());
-			ads.closeAdIfPresent(driver);
-			System.out.println("current page is: " + driver.getCurrentUrl());
-		}
-//pagination
-//Get list of Pages elements
-		List<WebElement> totalpages = driver.findElements(
-				By.xpath("//ul[@class='pagination justify-content-center align-items-center']//*[@class='page-item']"));
-		System.out.println("Total pages in pagination are:" + totalpages.size());
-		int lastpage = 1;
+    @Test
+    public void scrapeAndStoreRecipes() throws InterruptedException {
 
-//Extract highest page number
-		for (int i = 0; i < totalpages.size(); i++) {
-			WebElement page = totalpages.get(i);
-			String text = page.getText().trim();
-			if (text.matches("\\d+")) {
-				int pagenumber = Integer.parseInt(text);
-				if (pagenumber > lastpage) {
-					lastpage = pagenumber;
-				}
-			}
-		}
-		System.out.println("Total pages are: " + lastpage);
-//iterate all the pages
-		for (int pnumber = 1; pnumber <= lastpage; pnumber++) {
-			String pageUrl = "https://www.tarladalal.com/recipes/?page=" + pnumber;
-			driver.get(pageUrl);
-			// Fetch the parent web element
-			WebElement parentelement = driver
-					.findElement(By.xpath("//div[@class='col-md-12']//div[contains(@class, 'recipe-list')]"));
-			// Fetch all the recipes using the parent web element
-			List<WebElement> allrecipes = parentelement
-					.findElements(By.xpath("//div[@class='recipe-block brd-radius-5 mb-3']"));
-			// Iterate all recipes in current page
-			for (int i = 0; i < allrecipes.size(); i++) {
-				WebElement recipe = allrecipes.get(i);
-				try {
-					WebElement recipelink = wait.until(ExpectedConditions
-							.visibilityOf(recipe.findElement(By.xpath(".//h5[contains(@class, 'two-line-text')]/a"))));
+        // Find all recipe links       
+        WebElement searchIcon = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//i[@class='fa fa-search'])[1]")));
+        js.executeScript("arguments[0].click();", searchIcon);
+        
+        Set<String> seenHrefs = new HashSet<>();
+        List<String> allRecipeHrefs = collectAllRecipeLinks();
 
-					try {
-						recipelink.click(); // Try clicking
-					} catch (ElementClickInterceptedException e) {
-						// Scroll down and retry if not clickable
-						js.executeScript("arguments[0].scrollIntoView(true); window.scrollBy(0, 700);", recipelink);
-						Thread.sleep(1000); // Small wait for smooth scrolling, optional
-						recipelink.click(); // Retry clicking
-					}
+        for (String recipeHref : allRecipeHrefs) {        	                        
+            if (!seenHrefs.contains(recipeHref)) {
+            	seenHrefs.add(recipeHref);
+            	logger.info(recipeHref);
+                scrapeRecipeDetails(recipeHref);
+                //break;
+            }            
+        }
 
-				} catch (TimeoutException e) {
-					System.out.println("Recipe link not found or not visible in time.");
-				}
-				WebElement cuisinename = driver
-						.findElement(By.xpath("(//div[@class='col-md-9 content-body']//p//span)[2]/a"));
-				System.out.println("Cuisine: " + cuisinename.getText());
-				String Cuisine = cuisinename.getText();
-				// WebElement recipeheader=driver.findElement(By.xpath("//div[@class='col-md-9
-				// content-body']//p//span/a"));
-				List<WebElement> recipeHeaders = driver
-						.findElements(By.xpath("//div[@class='col-md-9 content-body']//p//span/a"));
-				StringBuilder result = new StringBuilder();
-				for (WebElement header : recipeHeaders) {
-					result.append(header.getText().trim()).append(" ");
-				}
-				System.out.println("Recipe Header:" + result.toString().trim());
-				// System.out.println("Recipe Header: "+recipeheader.getText());
-				WebElement recipename = driver.findElement(By.xpath("//h4[@class='rec-heading']/span"));
-				System.out.println("Recipe Name: " + recipename.getText());
-				WebElement recipedescription = driver.findElement(By.xpath("//div[@id='aboutrecipe']//p[strong]"));
-				System.out.println("Recipe description is: " + recipedescription.getText());
-				WebElement recipemethod = driver.findElement(By.xpath("//div[@id='methods']"));
-				System.out.println("Recipe Method: " + recipemethod.getText());
-				String recipeURL = driver.getCurrentUrl();
-				System.out.println("Recipe URL: " + recipeURL);
-				WebElement recipeIngredients = driver.findElement(By.xpath("//div[@id='ingredients']"));
-				System.out.println("Recipe Ingredients: " + recipeIngredients.getText());
-				WebElement nutrientvalue = driver.findElement(By.xpath("//div[@id='nutrients']"));
-				System.out.println("Nutritional value: " + nutrientvalue.getText());
-				WebElement Preptime = driver.findElement(By.xpath("(//div[@class='row border-bottom']/div/div/p)[1]"));
-				System.out.println("Prep time: " + Preptime.getText());
-				WebElement cooktime = driver.findElement(By.xpath("(//div[@class='row border-bottom']/div/div/p)[2]"));
-				System.out.println("Cooking time: " + cooktime.getText());
-				WebElement servings = driver.findElement(By.xpath("(//div[@class='row border-bottom']/div/div/p)[4]"));
-				System.out.println("No. of servings: " + servings.getText());
-				// go back to the allrecipes page
-				driver.navigate().back();
-			}
-		}
-
+        DB.insertRecipesIntoDatabase(allRecipes);
+    }
+    
+    @Test(priority = 2)
+	public void Queries() {		
+		
+    	Object Data[][] = ExcelReader.readExcelData("Final list for LCHFElimination ");
+		// To get Eliminate - Data[][0]
+    	// To get Add 		- Data[][1]
+    	    
 	}
+    
+    public List<String> collectAllRecipeLinks() throws InterruptedException {
+    	
+    	By recipeContainerLocator = By.xpath("(//div[@class='container'])[3]");
+        By recipeLinkInContainerLocator = By.xpath("//div[@class='img-block']/a");
+        By nextPageButtonLocator = By.xpath("//a[contains(text(), 'Next')]");
+        List<String> allHrefs = new ArrayList<>();
+        Set<String> uniqueHrefs = new HashSet<>();     
+        int count = 0;
 
+    	while (true) {            
+    		try {    			
+    			
+    			String pageSource = driver.getPageSource();
+    			if (pageSource.contains("Server Error")) {
+    			    logger.info("Server Error 500 encountered!");
+    			    String url = driver.getCurrentUrl();
+    			    int pageIndex = url.indexOf("&page=");
+    			    String baseUrl = url.substring(0, pageIndex + "&page=".length());
+    			    String pageNumberStr = url.substring(pageIndex + "&page=".length());
+    			    int currentPage = Integer.parseInt(pageNumberStr);
+    			    currentPage++;
+    			    url = baseUrl + currentPage;
+    			    driver.get(url);    			    
+    			}    			
+    			
+    			// 1. Wait for the recipe container to be present
+    			WebElement recipeContainer = wait.until(ExpectedConditions.presenceOfElementLocated(recipeContainerLocator));
+    		
+	            // 2. Find all recipe links within the container
+	            List<WebElement> recipeLinkElements = recipeContainer.findElements(recipeLinkInContainerLocator);
+	            if (count >= 51)	// start page
+	            for (WebElement linkElement : recipeLinkElements) {
+	                String href = linkElement.getAttribute("href");
+	                if (href != null && !href.isEmpty() && uniqueHrefs.add(href)) {
+	                    allHrefs.add(href);
+	                }
+	            }    		
+    		
+	            // 3. Try to find the "Next" page button            
+	    		WebElement nextPageButton = null;
+	            nextPageButton = driver.findElement(nextPageButtonLocator);
+	            if (nextPageButton == null)
+	            {
+	            	logger.info("No more 'Next' page found.");
+	                break;
+	            }
+	            else
+	            {	                        	
+	            	try {
+	            		js.executeScript("arguments[0].click();", nextPageButton);	                    	                    
+	                } catch (Exception e) {
+	                	logger.info("An error occurred while clicking 'Next': " + e.getMessage());
+	                }
+	            	count++;
+	            	if(count == 80) // No of pages to load
+	            		break;	
+	            }
+    		}
+    		catch (Exception e) {
+            	//Check for Ads
+    			checkforAd();
+            }            
+        }
+
+    	logger.info("Finished collecting all recipe links. Total unique: " + allHrefs.size());
+        return allHrefs;
+    }
+    
+    public void checkforAd() {
+    	try {
+			WebElement adOverlay = driver.findElement(By.xpath("//div[@id='card']"));
+			logger.info("Ad overlay is visible.");			
+	    	WebElement closeButton = adOverlay.findElement(By.xpath("//span[contains(text(), 'Close')]")); // Adjust locator
+	    	closeButton.click();
+		}
+		catch (Exception e) {
+        	//No ad visible
+			logger.info("Ad error: "+ e.getMessage());
+        }
+    }
+
+    public void scrapeRecipeDetails(String recipeURL) {    	
+        driver.get(recipeURL);
+        recipesdetailslocator recipe = new recipesdetailslocator();
+        recipe.setRecipeURL(recipeURL);
+
+        try {
+            recipe.setRecipeName(driver.findElement(By.xpath("(//div[@class='ingredients']/h4/span)[1]")).getText().replaceFirst("For ", ""));
+
+            // Recipe categories
+            String RecipeText = driver.findElement(By.xpath("//ul[@class='tags-list']")).getText().toLowerCase();
+            
+            if (RecipeText.contains("breakfast")) {
+                recipe.setRecipeCategory("Breakfast");
+            } else if (RecipeText.contains("lunch")) {
+                recipe.setRecipeCategory("Lunch");
+            } else if (RecipeText.contains("dinner")) {
+                recipe.setRecipeCategory("Dinner");
+            } else if (RecipeText.contains("snacks")) {
+                recipe.setRecipeCategory("Snacks");
+            } else {
+            	recipe.setRecipeCategory("Lunch");
+            }
+                   
+            // Ingredients
+            WebElement ingredientsElement = driver.findElement(By.id("ingredients"));
+            List<WebElement> ingredientListItems = ingredientsElement.findElements(By.tagName("p"));
+            StringBuilder ingredients = new StringBuilder();
+            for (WebElement li : ingredientListItems) {
+                ingredients.append(li.getText()).append("\n");
+            }
+            recipe.setIngredients(ingredients.toString().trim());
+            
+            // Food Category
+            String FoodText = recipe.getIngredients().toLowerCase();
+            
+            if (FoodText.contains("fish") || FoodText.contains("chicken") || FoodText.contains("meat")) {
+            	recipe.setFoodCategory("Non-veg");
+            }
+            else if (FoodText.contains("egg")) {
+            	recipe.setFoodCategory("Eggitarian");
+            }
+        	else if (FoodText.contains("butter") || FoodText.contains("ghee")) {
+        		recipe.setFoodCategory("Vegetarian");
+        	}            	
+        	else {            	
+        		recipe.setFoodCategory("Vegan");
+        	}
+            
+            // Preparation and Cooking Time
+            WebElement Preptime = driver.findElement(By.xpath("//*[text()='Preparation Time']/../p"));
+            recipe.setPreparationTime(Preptime.getText());
+            
+            try {
+            	WebElement Cooktime = driver.findElement(By.xpath("//*[text()='Cooking Time']/../p"));
+            	recipe.setCookingTime(Cooktime.getText());
+            }
+            catch(Exception e) {
+            	recipe.setCookingTime("25 Mins");
+            }
+            
+            // Tag
+            WebElement tagElement = driver.findElement(By.xpath("//*[text()='Tags']/../following-sibling::div/ul"));
+            List<WebElement> TagListItems = tagElement.findElements(By.tagName("li"));
+            StringBuilder Tags = new StringBuilder();
+            for (WebElement li : TagListItems) {
+                Tags.append(li.getText()).append("\n");
+            }
+            recipe.setTag(Tags.toString().trim());                         
+
+            // No of Servings
+            WebElement servingsElement = driver.findElement(By.xpath("//*[text()='Makes ']/../p"));
+            if (servingsElement != null) {
+                recipe.setNoOfServings(servingsElement.getText());
+            }
+
+            // Cuisine Category
+            try {
+	            WebElement cuisineElement = driver.findElement(By.xpath("//*[text()='Cuisine  ']/../following-sibling::span[1]/a"));
+	            if (cuisineElement != null) {
+	                recipe.setCuisineCategory(cuisineElement.getText());
+	            }
+            }
+            catch(Exception e){
+            	recipe.setCuisineCategory("Indian");  
+            }
+
+            // Recipe Description
+            WebElement descriptionElement = driver.findElement(By.id("aboutrecipe"));
+            List<WebElement> descListItems = descriptionElement.findElements(By.tagName("p"));
+            StringBuilder desc = new StringBuilder();
+            for (WebElement p : descListItems) {            	
+            	String paragraphContent = p.getText();
+                // Remove <strong> tags
+                paragraphContent = paragraphContent.replaceAll("<strong>", "").trim();
+                paragraphContent = paragraphContent.replaceAll("</strong>", "").trim();
+                if (!paragraphContent.isEmpty()) {
+                	desc.append(paragraphContent).append("\n\n");
+                }
+            }
+            
+            recipe.setRecipeDescription(desc.toString().trim());            
+
+            // Preparation Method
+            WebElement methodElement = driver.findElement(By.id("methods"));
+            List<WebElement> methodListItems = methodElement.findElements(By.tagName("li"));
+            StringBuilder method = new StringBuilder();
+            for (WebElement li : methodListItems) {
+                method.append(li.getText()).append("\n");
+            }
+            recipe.setPreparationMethod(method.toString().trim());
+
+            // Nutrient Values
+            try {
+	            WebElement nutrientsTable = driver.findElement(By.id("rcpnutrients"));
+	            WebElement nutrientsTbody = nutrientsTable.findElement(By.tagName("tbody"));
+	            List<WebElement> nutrientRows = nutrientsTbody.findElements(By.tagName("tr"));
+	
+	            StringBuilder nutrientValues = new StringBuilder();
+	            for (WebElement row : nutrientRows) {
+	                List<WebElement> cells = row.findElements(By.tagName("td"));
+	                if (cells.size() == 2) {
+	                    String nutrientName = cells.get(0).getText().trim();
+	                    String nutrientValue = cells.get(1).getText().trim();
+	                    nutrientValues.append(nutrientName).append(": ").append(nutrientValue).append("\n");
+	                }
+	            }
+	            recipe.setNutrientValues(nutrientValues.toString().trim());	            
+            }
+            catch(Exception e){
+            	recipe.setNutrientValues("");  
+            }
+
+            allRecipes.add(recipe);
+
+        } catch (Exception e) {
+        	logger.info("Error scraping details for: " + recipeURL + " - " + e.getMessage());
+        }
+    }
+    
+
+    @AfterMethod
+    public void tearDown() {
+        DB.close();
+       // if (driver != null) {
+       //     driver.quit();
+       // }
+    }
 }
